a37d4aec2badc12c025d4176d8dc19257c9378bf
Trey Davidson <treydavidson@Treys-MacBook-Air.local>
2024-11-16 23:51:47 -0600

    Simplified FIle Managment

--- Example.R
+++
@@ -1,8 +0,0 @@
-library(reticulate)
-source_python("~/Desktop/Statistical-Learning/Example.py")
-
-parameters <- c(lin_regress_one(c(1, 2, 3, 4), c(1, 2, 3, 4)))
-parameters <- as.numeric(parameters)
-
-
-
--- a.py
+++
@@ -1,4 +0,0 @@
-import example
-
-result = example.sum_of_squares(100)
-print(result)
